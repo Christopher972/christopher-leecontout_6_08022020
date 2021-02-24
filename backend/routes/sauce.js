be3ea2be2797011fccd34ee466234574ff803057
Christopher-Lee Contout <contout.christopher@hotmail.fr>
2021-02-24 13:13:09 +0100

    dernières modifications

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,12 +6,12 @@ const multer = require('../middleware/multer-config'); //// Importation du middl
 
 const sauceCtrl = require('../controllers/sauce');////// Importation de la logique métier ddes routes 
 
-router.get('/', auth, sauceCtrl.getAllSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router; //// Exportation du router du fichier 
 
